0fa4279ec7e8758edfb2f8e0f3caa3a115432d0e
Monikaselas <amonikaselas40@gmail.com>
2026-01-30 11:21:03 +0530

    Add files via upload

---
+++ task/script.js
@@ -0,0 +1,16 @@
+const data =[
+    {
+         city: "Chennai", temperature: "32°C", condition: "Sunny" },
+  { city: "Bangalore", temperature: "26°C", condition: "Cloudy" },
+  { city: "Delhi", temperature: "18°C", condition: "Foggy" }
+    
+];
+const a=document.getElementById("a");
+const b=document.getElementById("b");
+const c=document.getElementById("c");
+
+a.addEventListener(click,()=> {
+    const d=d.value.trim();
+})
+
+    
